2f88a4820a90ac047812f95b98dd0b9928771c01
Kjetil Harneshaug Hauger <kjehar01278@stud.noroff.no>
2024-12-15 16:53:15 +0100

    FIX

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,37 +1,18 @@
-import jsPlugin from '@eslint/js';
-import globals from 'globals';
-
-export default [
-  {
-    files: ['**/*.js'], 
-    languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
-      globals: {
-        ...globals.browser, 
+export default {
+  content: ['./**/*.{html,js}', '!./node_modules/**/*'], 
+  theme: {
+    extend: {
+      colors: {
+        primary: '#006494',
+        secondary: '#051923',
+        tertiary: '#003554',
+        white: '#ffffff',
+      },
+      fontFamily: {
+        header: ['Montserrat', 'sans-serif'], 
+        body: ['Lato', 'sans-serif'],
       },
     },
   },
-  jsPlugin.configs.recommended,
-  {
-    rules: {
-      'quotes': ['error', 'single'], 
-      'semi': ['error', 'always'], 
-      'no-unused-vars': 'warn', 
-      'no-console': 'off', 
-      'indent': ['error', 2], 
-      'comma-dangle': ['error', 'always-multiline'],
-      'object-curly-spacing': ['error', 'always'], 
-      'array-bracket-spacing': ['error', 'never'], 
-      'key-spacing': ['error', { beforeColon: false, afterColon: true }], 
-    },
-  },
-  {
-    files: ['tailwind.config.js'], 
-    languageOptions: {
-      globals: {
-        ...globals.node,
-      },
-    },
-  },
-];
+  plugins: [],
+};
